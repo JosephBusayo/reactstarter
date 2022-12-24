e0a621fb46a652ba0b8ca4657cb0d8e204ae1949
Joseph Jayeoba <josephbusayojayeoba@gmail.com>
2022-12-24 17:17:14 +0100

    react form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 /* import UseEffect from './tutorial/2-useEffect/setup/1-useEffect-basics' */
 /* import UseEffect from './tutorial/2-useEffect/setup/2-useEffect-cleanup' */
-import UseEffect from './tutorial/2-useEffect/setup/3-useEffect-fetch-data'
+/* import UseEffect from './tutorial/2-useEffect/setup/3-useEffect-fetch-data' */
+import ControlledInputs from './tutorial/4-forms/setup/2-multiple-inputs'
 
 
 function App() {
   return (
     <div className='container'>
-      <UseEffect />
+      <ControlledInputs />
     </div>
   )
 }
